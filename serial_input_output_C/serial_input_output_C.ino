c77b2ec7b41cb7aa3b42cbdf4e47297df6da6e2a
HardUIKnow <Harduiknow@gmail.com>
2014-08-15 18:35:58 +0200

    Added comments on the sketch

--- serial_input_output_C/serial_input_output_C.ino
+++ serial_input_output_C/serial_input_output_C.ino
@@ -1,9 +1,22 @@
+/* Arduino Serial Input / output
+* exemple in real C.
+* In this file you can view how to get started with
+* USART Init
+* USART Received/Send data
+* USART PutString
+* How to use it : A led is connected to (arduino pin 13) PINB5
+* Sending 'H' via serial will pull on the led
+* Sending anything else will pull off the led
+*/
+
+// Include and CPU defines
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define BAUDRATE 9600
-#define BAUD_PRESCALLER (((F_CPU / (BAUDRATE * 16UL))) - 1)
+// BAUDRATE and prescaler definition
+#define BAUDRATE 9600										//Baudrate to 9600
+#define BAUD_PRESCALLER (((F_CPU / (BAUDRATE * 16UL))) - 1)	//Baudrate configuration
 
 //Declaration of our functions
 void USART_init(void);
@@ -14,18 +27,18 @@ void USART_putstring(char* StringPtr);
 char String[]="Hello world!!";    //String[] is in fact an array but when we put the text between the " " symbols the compiler threats it as a String and automatically puts the null termination character in the end of the text
 char receiveByte;
 
-
+// main function
 int main(void){
-	USART_init();        //Call the USART initialization code
-	USART_putstring(String);
-        DDRB |= (1<<PINB5);
-	while(1){        //Infinite loop
-	 receiveByte = USART_receive();
-	 USART_send(receiveByte);
-	 if(receiveByte == 'H'){
-            PORTB |= (1<<PINB5);
+	USART_init();        				//Call the USART initialization code
+	USART_putstring(String);			//Sending the "hello world" String
+        DDRB |= (1<<PINB5);				//Pin 5 of Port B pushed to OUTPUT
+	while(1){        					//Infinite loop
+	 receiveByte = USART_receive();		//Check received Byte
+	 USART_send(receiveByte);			//Echo the received byte
+	 if(receiveByte == 'H'){			//Checking if received byte is an 'H'
+            PORTB |= (1<<PINB5);		//if so pull led on
           } else {
-            PORTB &= ~(1<<PINB5);
+            PORTB &= ~(1<<PINB5);		//else pull it off
           }
 }
 
@@ -44,14 +57,12 @@ unsigned char USART_receive(void){
 
  while(!(UCSR0A & (1<<RXC0)));
  return UDR0;
-
 }
 
 void USART_send( unsigned char data){
 
  while(!(UCSR0A & (1<<UDRE0)));
  UDR0 = data;
-
 }
 
 void USART_putstring(char* StringPtr){
@@ -59,5 +70,4 @@ void USART_putstring(char* StringPtr){
 while(*StringPtr != 0x00){
  USART_send(*StringPtr);
  StringPtr++;}
-
 }
